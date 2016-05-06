8a4210f93b2757df189020aeb8e19eec3a1ca8c8
Yoav Kleinberger <haarcuba@gmail.com>
2016-05-07 01:38:12 +0300

    need test-selectors for lein test :selector to work

--- carly/project.clj
+++ carly/project.clj
@@ -8,4 +8,16 @@
                  [clj-yaml "0.4.0"]
                  [clojurewerkz/cassaforte "2.1.0-beta1"]
                  [clj-ssh "0.5.14"]
-                 [jkni/jepsen "0.0.7-SNAPSHOT"] ])
+                 [jkni/jepsen "0.0.7-SNAPSHOT"] ]
+  :test-selectors {:steady :steady
+                   :bootstrap :bootstrap
+                   :map :map
+                   :set :set
+                   :mv :mv
+                   :batch :batch
+                   :lwt :lwt
+                   :decommission :decommission
+                   :counter :counter
+                   :clock :clock
+                   :slow-network :slow-network
+                   :all (constantly true)})
