7d8397a4326a5c0e98a8c6b6aa5f1451ce45f175
Yoav Kleinberger <yoav@scylladb.com>
2016-05-08 18:03:17 +0300

    support junit output format

--- carly/project.clj
+++ carly/project.clj
@@ -3,9 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [ [org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jmx "0.3.1"]
                  [clj-yaml "0.4.0"]
                  [clojurewerkz/cassaforte "2.1.0-beta1"]
                  [clj-ssh "0.5.14"]
-                 [jkni/jepsen "0.0.7-SNAPSHOT"] ])
+                 [jkni/jepsen "0.0.7-SNAPSHOT"] ]
+  :plugins      [ [test2junit "1.2.2"] ]
+  :test2junit-output-dir "junit-report"
+  :test2junit-run-ant true)
